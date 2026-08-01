62c1fcfbd60a00b8c7da1b764fc3c7212debdd49
Laby Damaro CAMARA <labydamarocamara87@gmail.com>
2026-08-01 18:15:59 +0000

    Format commit - Documents FORMAT DES TABLEAUX

--- commit_git.bat
+++ commit_git.bat
@@ -5,7 +5,7 @@ chcp 65001 > nul
 git add .
 
 :: 4. Enregistre le premier instantané (snapshot)
-git commit -m "Traduction commit - Documents ODD Nations Unis VF"
+git commit -m "Format commit - Documents FORMAT DES TABLEAUX"
 
 
 :: 6. Envoie votre code sur GitHub et définit "main" comme branche par défaut
