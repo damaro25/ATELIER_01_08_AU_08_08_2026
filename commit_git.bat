6a1e9dc7902988383ca0ea106eb5d9d25fe33cfb
Laby Damaro CAMARA <labydamarocamara87@gmail.com>
2026-08-01 16:28:02 +0000

    Dictionnaire commit - Documents Dictionnaire Ménage Ordinaire RGPH-4

--- commit_git.bat
+++ commit_git.bat
@@ -5,7 +5,7 @@ chcp 65001 > nul
 git add .
 
 :: 4. Enregistre le premier instantané (snapshot)
-git commit -m "Traduction commit - Documents ODD Nations Unis"
+git commit -m "Dictionnaire commit - Documents Dictionnaire Ménage Ordinaire RGPH-4"
 
 
 :: 6. Envoie votre code sur GitHub et définit "main" comme branche par défaut
