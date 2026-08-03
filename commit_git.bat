023e81458268a1eceb84e32c8fa5733fb3a7fd2c
Laby Damaro CAMARA <labydamarocamara87@gmail.com>
2026-08-03 13:39:29 +0000

    Agenda 2063 Union africaine

--- commit_git.bat
+++ commit_git.bat
@@ -5,7 +5,7 @@ chcp 65001 > nul
 git add .
 
 :: 4. Enregistre le premier instantané (snapshot)
-git commit -m "Calcul de la prévalence"
+git commit -m "Agenda 2063 Union africaine"
 
 
 :: 6. Envoie votre code sur GitHub et définit "main" comme branche par défaut
